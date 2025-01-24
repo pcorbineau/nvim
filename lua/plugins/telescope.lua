778094e56d3565f33513818fb6a3275b31c0319d
Paul Corbineau <paul.corbineau@blade-group.com>
2025-01-24 15:52:04 +0100

    update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = { 'nvim-lua/plenary.nvim' },
+	cmd = { "Telescope" },
 	keys = {
 		{ "<leader>ff",  function() require("telescope.builtin").find_files() end,                                                   desc = "Find Files" },
 		{ "<leader>fg",  function() require("telescope.builtin").live_grep() end,                                                    desc = "Live Grep" },
