69c4e8ac711ef3c8e6adf52249da92761fe5a249
Paul Corbineau <paul.corbineau@blade-group.com>
2025-02-09 20:30:38 +0100

    last update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,41 +3,45 @@ local general = vim.api.nvim_create_augroup("General", { clear = true })
 
 -- disable auto comment on newline
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "*",
-	command = "set formatoptions-=cro",
-	group = general,
-	desc = "Disable new line comment",
+    pattern = "*",
+    command = "set formatoptions-=cro",
+    group = general,
+    desc = "Disable new line comment",
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client.supports_method('textDocument/implementation') then
-			-- Create a keymap for vim.lsp.buf.implementation
-			local opts = { buffer = args.buf }
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        local bufnr = args.buf
 
-			vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-			vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-			vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-			vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-			vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-			vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-			vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-			vim.keymap.set({ 'n', 'x' }, '<leader>lf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>',
-				opts)
-			vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-			vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-			vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-			vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
-		end
-		if client.supports_method('textDocument/formatting') then
-			-- Format the current buffer on save
-			vim.api.nvim_create_autocmd('BufWritePre', {
-				buffer = args.buf,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-				end,
-			})
-		end
-	end,
+        local nmap = function(keys, func, desc)
+            if desc then
+                desc = "LSP: " .. desc
+            end
+            vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+        end
+
+        local cnmap = function(method, keys, func, desc)
+            if client and client.supports_method(method) then
+                nmap(keys, func, desc)
+            end
+        end
+        -- Create a keymap for vim.lsp.buf.implementation
+        cnmap("textDocument/hover", 'K', vim.lsp.buf.hover, "Hover Docs")
+        cnmap("textDocument/hover", 'gd', vim.lsp.buf.definition, "[g]oto [d]efinition")
+        cnmap("textDocument/hover", 'gD', vim.lsp.buf.declaration, "[g]oto [D]eclaration")
+        cnmap("textDocument/hover", 'gi', vim.lsp.buf.implementation, "[g]oto [i]mplementation")
+        cnmap("textDocument/hover", 'go', vim.lsp.buf.type_definition, "Type [de]finition")
+        cnmap("textDocument/hover", '<leader>rn', vim.lsp.buf.rename, "[r]e[n]ame")
+        cnmap("textDocument/hover", '<leader>ca', vim.lsp.buf.code_action, "[c]ode [a]ction")
+        if client and client.supports_method('textDocument/formatting') then
+            -- Format the current buffer on save
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                end,
+            })
+        end
+    end,
 })
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,65 +1,65 @@
 return {
-	"neovim/nvim-lspconfig",
-	dependencies = {
-		{
-			"williamboman/mason-lspconfig.nvim",
-			dependencies = {
-				{ "williamboman/mason.nvim", opts = {} },
-			},
-			opts = {
-				automatic_installation = true,
-				ensure_installed = {
-					"clangd",
-					"lua_ls",
-					"neocmake",
-					"pyright",
-				},
-				handlers = {
-					function(server_name)
-						local capabilities = require('blink.cmp').get_lsp_capabilities()
-						require("lspconfig")[server_name].setup { capabilities = capabilities }
-					end,
-					["lua_ls"] = function()
-						local capabilities = require('blink.cmp').get_lsp_capabilities()
-						require("lspconfig")["lua_ls"].setup {
-							on_init = function(client)
-								if client.workspace_folders then
-									local path = client.workspace_folders[1].name
-									if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-										return
-									end
-								end
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        {
+            "williamboman/mason-lspconfig.nvim",
+            dependencies = {
+                { "williamboman/mason.nvim", opts = {} },
+            },
+            opts = {
+                automatic_installation = true,
+                ensure_installed = {
+                    "clangd",
+                    "lua_ls",
+                    "neocmake",
+                    "pyright",
+                },
+                handlers = {
+                    function(server_name)
+                        local capabilities = require('blink.cmp').get_lsp_capabilities()
+                        require("lspconfig")[server_name].setup { capabilities = capabilities }
+                    end,
+                    ["lua_ls"] = function()
+                        local capabilities = require('blink.cmp').get_lsp_capabilities()
+                        require("lspconfig")["lua_ls"].setup {
+                            on_init = function(client)
+                                if client.workspace_folders then
+                                    local path = client.workspace_folders[1].name
+                                    if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+                                        return
+                                    end
+                                end
 
-								client.config.settings.Lua = vim.tbl_deep_extend('force',
-									client.config.settings.Lua, {
-										runtime = {
-											-- Tell the language server which version of Lua you're using
-											-- (most likely LuaJIT in the case of Neovim)
-											version = 'LuaJIT'
-										},
-										-- Make the server aware of Neovim runtime files
-										workspace = {
-											checkThirdParty = false,
-											library = {
-												vim.env.VIMRUNTIME
-												-- Depending on the usage, you might want to add additional paths here.
-												-- "${3rd}/luv/library"
-												-- "${3rd}/busted/library",
-											}
-											-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
-											-- library = vim.api.nvim_get_runtime_file("", true)
-										}
-									})
-							end,
-							settings = {
-								Lua = {}
-							},
-							capabilities = capabilities,
-						}
-					end,
-				}
-			},
-		},
-		"saghen/blink.cmp", -- to remove from neovim 0.11+
-	},
+                                client.config.settings.Lua = vim.tbl_deep_extend('force',
+                                    client.config.settings.Lua, {
+                                        runtime = {
+                                            -- Tell the language server which version of Lua you're using
+                                            -- (most likely LuaJIT in the case of Neovim)
+                                            version = 'LuaJIT'
+                                        },
+                                        -- Make the server aware of Neovim runtime files
+                                        workspace = {
+                                            checkThirdParty = false,
+                                            library = {
+                                                vim.env.VIMRUNTIME
+                                                -- Depending on the usage, you might want to add additional paths here.
+                                                -- "${3rd}/luv/library"
+                                                -- "${3rd}/busted/library",
+                                            }
+                                            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                                            -- library = vim.api.nvim_get_runtime_file("", true)
+                                        }
+                                    })
+                            end,
+                            settings = {
+                                Lua = {}
+                            },
+                            capabilities = capabilities,
+                        }
+                    end,
+                }
+            },
+        },
+        "saghen/blink.cmp", -- to remove from neovim 0.11+
+    },
 }
